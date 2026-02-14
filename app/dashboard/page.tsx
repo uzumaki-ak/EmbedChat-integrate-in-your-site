853d61fa9e7d7fb20db8d0d6fcfa0feba317f0c2
uzumaki-ak <anikydv38@gmail.com>
2026-02-14 15:38:52 +0530

    Implemented dashboard layout and a multi-step initial form to collect business name, website, and external links.

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import InitialForm from "@/components/dashboard/initialForm";
+
+function DashboardPage() {
+  return (
+    <div className='flex-1 flex-w-full '>
+        <InitialForm />
+    </div>
+  )
+}
+
+export default DashboardPage                    
